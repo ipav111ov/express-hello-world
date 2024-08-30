98b4f3c71a63d12f6e3807971cb4f83ef1acc700
ipav111ov <152607614+ipav111ov@users.noreply.github.com>
2024-08-30 09:27:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get("/", (req, res) => res.type('html').send(html)); 
+
+// Add a new route for the health endpoint 
+
+app.get("/health", (req, res) => { 
+
+  res.sendStatus(200); 
+
+}); 
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
